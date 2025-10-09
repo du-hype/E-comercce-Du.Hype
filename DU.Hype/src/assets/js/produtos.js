87c4ff32e73546bc27e94c692cc2e4e8a83faf5f
Davi1901 <davibitencourt1901@gmail.com>
2025-10-09 16:54:33 -0300

    Praticamente finalizado

---
+++ DU.Hype/src/assets/js/produtos.js
@@ -0,0 +1,58 @@
+// Arquivo: produtos.js
+
+const produtos = [
+    {
+        id: 1,
+        marca: "PUMA",
+        nome: "Puma 180 Strong Gray",
+        preco: "R$ 950,99",
+        imagem_principal: "/DU.Hype/src/assets/Images/puma180_strong_gray.png",
+        imagens_thumb: [
+            "/DU.Hype/src/assets/Images/puma180_strong_gray.png",
+            // Adicione mais caminhos de imagens de thumbnail aqui se tiver
+        ],
+        descricao: "Um tênis robusto com design inspirado nos anos 2000, oferecendo conforto e estilo para o dia a dia."
+    },
+    {
+        id: 2,
+        marca: "NIKE",
+        nome: "Dunk Nature Blue Tint",
+        preco: "R$ 947,97",
+        imagem_principal: "/DU.Hype/src/assets/Images/tenis_dunk_low-blue.png",
+        imagens_thumb: [],
+        descricao: "O clássico do basquete dos anos 80, repaginado com materiais sustentáveis e um esquema de cores refrescante."
+    },
+    {
+        id: 3,
+        marca: "ADIDAS",
+        nome: "Dime x Adidas ADI2000",
+        preco: "R$ 1.169,00",
+        imagem_principal: "/DU.Hype/src/assets/Images/dimeexadidas_adi2000.png",
+        imagens_thumb: [
+            "/DU.Hype/src/assets/Images/19.jpg",
+            "/DU.Hype/src/assets/Images/20.jpg",
+            "/DU.Hype/src/assets/Images/21.jpg",
+            "/DU.Hype/src/assets/Images/22.jpg"
+        ],
+        descricao: "A collab com a Dime coloca o espírito do skate do início dos anos 2000 à prova, com resistência impressionante e um design cheio de atitude."
+    },
+    {
+        id: 4,
+        marca: "ADIDAS",
+        nome: "Yeezy Boost 350 V2 Onyx",
+        preco: "R$ 597,90",
+        imagem_principal: "/DU.Hype/src/assets/Images/tenis_adidasYeezy.png",
+        imagens_thumb: [],
+        descricao: "Conforto icônico e um design minimalista, o Yeezy Boost 350 V2 na cor Onyx é uma peça essencial para qualquer coleção."
+    },
+    {
+        id: 5,
+        marca: "NIKE",
+        nome: "Air Max 90",
+        preco: "R$ 999,99",
+        imagem_principal: "/DU.Hype/src/assets/Images/air max 90 - 2.jpg",
+        imagens_thumb: [],
+        descricao: "Com o amortecimento Air visível que o tornou famoso, o Air Max 90 celebra um ícone com estilo e conforto duradouros."
+    }
+    // Adicione mais produtos aqui...
+];
